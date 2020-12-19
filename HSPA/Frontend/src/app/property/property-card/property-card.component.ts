1e54aafc0f6dd89bf7a5867a0b16f4277da16162
Abner Pena <abnrr.pena@gmail.com>
2020-12-19 19:53:13 -0400

    property list

--- HSPA/Frontend/src/app/property/property-card/property-card.component.ts
+++ HSPA/Frontend/src/app/property/property-card/property-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-property-card',
@@ -7,13 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyCardComponent implements OnInit {
 
-  Property:any = {
-    "id":1,
-    "Name":"Valle House",
-    "Type":"House",
-    "Price":120000
-  }
-
+  @Input() property : any
 
   constructor() { }
 
