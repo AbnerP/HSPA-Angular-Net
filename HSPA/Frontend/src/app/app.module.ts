f3e2196990c6f45525676b792808ecbb946bd620
Abner Pena <abnrr.pena@gmail.com>
2020-12-20 12:01:28 -0400

    Basic Routing🚏

--- HSPA/Frontend/src/app/app.module.ts
+++ HSPA/Frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {Routes, RouterModule} from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
@@ -7,18 +8,32 @@ import {PropertyCardComponent} from './property/property-card/property-card.comp
 import {PropertyListComponent} from './property/property-list/property-list.component'
 import {HttpClientModule} from '@angular/common/http'
 import { HousingService } from './services/housing.service';
+import {AddPropertyComponent} from './property/add-property/add-property.component';
 
+//Each route is a JS object
+const appRoutes: Routes = [
+  {
+    path: 'add-property',
+    component: AddPropertyComponent
+  },
+  {
+    path: '',
+    component: PropertyListComponent
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
       NavBarComponent,
       PropertyCardComponent,
-      PropertyListComponent
+      PropertyListComponent,
+      AddPropertyComponent
    ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [
     HousingService
