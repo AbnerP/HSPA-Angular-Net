5d691fc345097169ce5b48524c0716c81fb5f92a
Abner Pena <abnrr.pena@gmail.com>
2020-12-19 20:44:17 -0400

    properties.json/housingService implementation

--- HSPA/Frontend/src/app/app.module.ts
+++ HSPA/Frontend/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import {PropertyCardComponent} from './property/property-card/property-card.component'
 import {PropertyListComponent} from './property/property-list/property-list.component'
+import {HttpClientModule} from '@angular/common/http'
+import { HousingService } from './services/housing.service';
 
 
 @NgModule({
@@ -15,9 +17,12 @@ import {PropertyListComponent} from './property/property-list/property-list.comp
       PropertyListComponent
    ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    HousingService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- HSPA/Frontend/src/app/property/property-list/property-list.component.ts
+++ HSPA/Frontend/src/app/property/property-list/property-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {HousingService} from 'src/app/services/housing.service'
 
 @Component({
   selector: 'app-property-list',
@@ -6,48 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./property-list.component.css']
 })
 export class PropertyListComponent implements OnInit {
-  properties: Array<any> = [
-    {
-      "Id":1,
-      "Name":"Valle House",
-      "Type":"House",
-      "Price":250000
-    },
-    {
-      "Id":2,
-      "Name":"Isla Verde",
-      "Type":"Apartment",
-      "Price":400000
-    },
-    {
-      "Id":3,
-      "Name":"100 Elmwood Ave",
-      "Type":"House",
-      "Price":150000
-    },
-    {
-      "Id":4,
-      "Name":"Barton Street",
-      "Type":"House",
-      "Price":125000
-    },
-    {
-      "Id":5,
-      "Name":"Wilder 404",
-      "Type":"Apartment",
-      "Price":100000
-    },
-    {
-      "Id":6,
-      "Name":"Coco's Place",
-      "Type":"House",
-      "Price":175000
-    }
-  ]
+  properties: any;
 
-  constructor() { }
+  constructor(private  housingService:HousingService) { }
 
   ngOnInit() {
+    this.housingService.getAllProperties().subscribe(data=>{
+      this.properties=data;
+      console.log(data)
+    }, error =>console.log(error)
+    )
+
+    /*this.http.get('data/properties.json').subscribe(
+      data=>{
+        this.properties=data;
+        console.log(data)
+      });*/
+
   }
 
 }
